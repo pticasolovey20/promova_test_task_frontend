bf646e08988e9aec75a6cd03bda702e5c7e173c9
Dmytro Solovyov <dy.solovyov.work@gmail.com>
2024-12-18 19:52:25 +0200

    fix media url env

--- utils/getStrapiBaseUrl.ts
+++ utils/getStrapiBaseUrl.ts
@@ -1,3 +1,3 @@
 export const getStrapiBaseUrl = () => process.env.STRAPI_BASE_URL || 'http://localhost:1337';
 
-export const getStrapiBaseMediaUrl = () => process.env.STRAPI_BASE_MEADIA_URL || 'http://localhost:1337';
+export const getStrapiBaseMediaUrl = () => process.env.STRAPI_BASE_MEDIA_URL || 'http://localhost:1337';
